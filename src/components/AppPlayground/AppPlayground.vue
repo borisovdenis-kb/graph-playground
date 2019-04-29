4f2e6813462961eb55864d4a3b288bd35ab6dee5
Denis Borisov <borisovdenis-kb@yandex.ru>
2019-04-29 22:39:06 +0400

    refactor(12): add component for edge

--- src/components/AppPlayground/AppPlayground.vue
+++ src/components/AppPlayground/AppPlayground.vue
@@ -8,37 +8,7 @@
          @mousedown="onPgMousedown"
          @mouseup="onPgMouseup">
 
-      <g v-for="edge in edgeList"> <!-- TODO: вынести в компоненты -->
-        <line :id="edge.edgeId"
-              :x1="edge.x1"
-              :y1="edge.y1"
-              :x2="edge.x2"
-              :y2="edge.y2"
-              :cursor="edgeCursor"
-              :stroke="edgeStrokeColor(edge.isHighlighted)"
-              stroke-width="5"> <!-- TODO: проверить, что все нормально с курсором-->
-        </line>
-        <rect v-if="isEdgeWeightVisible"
-              :id="edge.edgeId"
-              :x="calcMiddleCoordinate(edge.x1, edge.x2) - 10"
-              :y="calcMiddleCoordinate(edge.y1, edge.y2) - 10"
-              width="35px"
-              height="20px"
-              fill="#f0f0f0"
-              style="opacity: 0.9"
-              rx="4"
-              ry="4">
-        </rect>
-
-        <text v-if="isEdgeWeightVisible"
-              :id="edge.edgeId"
-              :x="calcMiddleCoordinate(edge.x1, edge.x2) + 7"
-              :y="calcMiddleCoordinate(edge.y1, edge.y2) + 5"
-              text-anchor="middle"
-              fill="#a5a5a5">
-          {{edge.weight || 0}}
-        </text>
-      </g>
+      <app-edge v-for="edge in edgeList" v-bind:data="edge"></app-edge>
 
       <g v-for="vertex in vertexList" :cursor="vertexCursor">
         <circle :id="vertex.vertexId"
@@ -83,9 +53,11 @@
   import { isEventOnEntity, createMultiCommandObject } from "../../services/utils";
   import { algorithmMap } from "../../constants/algorithms";
   import { mapState } from 'vuex';
+  import AppEdge from "../AppEdge/AppEdge";
 
   export default {
     name: "app-playground",
+    components: {AppEdge},
     data() {
       return {
         pgWidth: 0,
@@ -242,9 +214,6 @@
       textStrokeColor(vertexId) {
         return vertexId === this.firstSelectedVertexId ? '#d7d7d7' : '#6062bf';
       },
-      edgeStrokeColor(isHighlighted) {
-        return isHighlighted ? '#ff822a' : '#c3c3c3';
-      },
       checkRedoIsEmpty() {
         const vm = this;
 
@@ -264,9 +233,6 @@
           command,
           {root: true}
         );
-      },
-      calcMiddleCoordinate(c1, c2) {
-        return (c1 + c2) / 2;
       }
     },
     computed: {
@@ -280,14 +246,6 @@
           [pgStates.MOVE_VERTEX]: 'move',
           [pgStates.DELETE_VERTEX]: 'pointer',
           [pgStates.ADD_EDGE]: 'pointer',
-          [pgStates.DELETE_EDGE]: 'pointer'
-        };
-        const currentPgState = this.$store.state.currentPgState;
-
-        return mapStateToCursor[currentPgState];
-      },
-      edgeCursor() {
-        const mapStateToCursor = {
           [pgStates.DELETE_EDGE]: 'pointer'
         };
         const currentPgState = this.$store.state.currentPgState;
